c4c3299081f917d39b3b4321341d806882c0df5a
海绵宝宝的菠萝屋 <1394473521@qq.com>
2021-10-02 09:03:44 +0800

    Initial commit

---
+++ src/com/designpattern/tanke/T.java
@@ -0,0 +1,13 @@
+package com.designpattern.tanke;
+
+/**
+ * @Author: ZhangYiMing
+ * @CreateDate: 2021/10/2 8:29
+ * ^
+ * @Description:
+ **/
+public class T {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
