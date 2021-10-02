d91ea793b5a04e0f50cdded7e1f1570dc4296c06
海绵宝宝的菠萝屋 <1394473521@qq.com>
2021-10-02 18:05:35 +0800

    子弹连发并打出连发，同时对子弹进行边缘检测，超出范围进行删除

--- src/com/designpattern/tank/Bullet.java
+++ src/com/designpattern/tank/Bullet.java
@@ -10,28 +10,35 @@ import java.awt.*;
  **/
 public class Bullet {
     //子弹的宽高
-    private static final int WIDTH = 5,HEIGHT = 5;
+    private static final int WIDTH = 15, HEIGHT = 15;
     //子弹初始位置
     private int x = 200, y = 200;
     //子弹方向
     private Dir dir;
     //子弹速度
-    private static final int SPEED = 10;
+    private static final int SPEED = 20;
+    //是否存活
+    private boolean live = true;
+    TankFrame tankFrame;
 
-
-    public Bullet(int x, int y, Dir dir) {
+    public Bullet(int x, int y, Dir dir,TankFrame tankFrame) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tankFrame =tankFrame;
     }
 
     public void paint(Graphics g) {
+        if (!live){
+              tankFrame.bullets.remove(this);
+        }
         Color color = g.getColor();
         g.setColor(Color.RED);
-        g.fillOval(x,y,WIDTH,HEIGHT);
+        g.fillOval(x, y, WIDTH, HEIGHT);
         g.setColor(color);
         move();
     }
+
     private void move() {
         switch (dir) {
             case LEFT:
@@ -47,5 +54,10 @@ public class Bullet {
                 y += SPEED;
                 break;
         }
+        if (x < 0 || y < 0 || x > TankFrame.GAME_HEIGHT || y > TankFrame.GAME_WIDTH){
+            live = false;
+        }
+
+
     }
 }
--- src/com/designpattern/tank/TankFrame.java
+++ src/com/designpattern/tank/TankFrame.java
@@ -5,6 +5,8 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Author: ZhangYiMing
@@ -14,10 +16,13 @@ import java.awt.event.WindowEvent;
  **/
 public class TankFrame extends Frame {
 
-    Tank myTank = new Tank(200, 200, Dir.DOWN);
-    Bullet bullet =new Bullet(300,300,Dir.UP);
+    Tank myTank = new Tank(200, 200, Dir.DOWN, this);
+    List<Bullet> bullets = new ArrayList();
+    Bullet bullet = new Bullet(300, 300, Dir.UP,this);
+    static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
+
     public TankFrame() {
-        setSize(800, 800);
+        setSize(GAME_WIDTH, GAME_HEIGHT);
         setResizable(false);
         setTitle("坦克大战");
         setVisible(true);
@@ -32,10 +37,38 @@ public class TankFrame extends Frame {
         });
     }
 
+    /**
+     * 、
+     * 解决屏幕闪烁：双缓存
+     * 在内存中创建一张图片，并定义一只画笔（graphics）都画完后统一交给屏幕（显存）
+     * 继承Frame 后 重写update 会截获paint 方法 需要在update内调用
+     */
+    Image offScreenImage = null;
+
+    @Override
+    public void update(Graphics g) {
+        if (offScreenImage == null) {
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+        Graphics graphics = offScreenImage.getGraphics();
+        Color color = g.getColor();
+        graphics.setColor(Color.BLACK);
+        graphics.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        graphics.setColor(color);
+        paint(graphics);
+        g.drawImage(offScreenImage, 0, 0, null);
+    }
+
     @Override
     public void paint(Graphics g) {
+        Color c = g.getColor();
+        g.setColor(Color.white);
+        g.drawString("子弹数量："+bullets.size(),10,60);
+        g.setColor(c);
         myTank.paint(g);
-        bullet.paint(g);
+        for (int i = 0; i < bullets.size(); i++) {
+            bullets.get(i).paint(g);
+        }
     }
 
     class MykeyListener extends KeyAdapter {
@@ -61,6 +94,8 @@ public class TankFrame extends Frame {
                 case KeyEvent.VK_DOWN:
                     keyDOWN = true;
                     break;
+                case KeyEvent.VK_SPACE:
+                    myTank.fire();
                 default:
                     break;
             }
@@ -93,7 +128,7 @@ public class TankFrame extends Frame {
 
             if (!keyLEFT && !keyUP && !keyRIGHT && !keyDOWN) {
                 myTank.setMoving(false);
-            }else{
+            } else {
                 myTank.setMoving(true);
                 if (keyLEFT) myTank.setDir(Dir.LEFT);
                 if (keyUP) myTank.setDir(Dir.UP);
